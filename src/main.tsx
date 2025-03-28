f90362c42643b78ccd420f56f972d4c7244156e8
김태성 <qwaszx82@danbiedu.co.kr>
2025-03-28 14:14:16 +0900

    tailwindcss적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <div className="bg-blue-500 text-white p-4 rounded-lg shadow-lg">
+      Hello, Tailwind CSS!
+    </div>
+  </StrictMode>
+);
